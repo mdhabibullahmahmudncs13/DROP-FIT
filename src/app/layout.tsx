ec457f26a036ff062bc925e3b74620c0fd9ee644
MD Habibullah Mahmud <mdhabibullahmahmudncs13@gmail.com>
2026-03-02 12:28:24 -0500

    meta Pixel added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+import Script from 'next/script';
 import { AuthProvider } from '@/context/AuthContext';
 import { CartProvider } from '@/context/CartContext';
 import { ToastProvider } from '@/components/ui/Toast';
@@ -28,7 +29,32 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      {/* Meta Pixel Code */}
+      <Script id="meta-pixel" strategy="afterInteractive">
+        {`
+          !function(f,b,e,v,n,t,s)
+          {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+          n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+          if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+          n.queue=[];t=b.createElement(e);t.async=!0;
+          t.src=v;s=b.getElementsByTagName(e)[0];
+          s.parentNode.insertBefore(t,s)}(window, document,'script',
+          'https://connect.facebook.net/en_US/fbevents.js');
+          fbq('init', '1287535843253689');
+          fbq('track', 'PageView');
+        `}
+      </Script>
+      {/* End Meta Pixel Code */}
       <body className={inter.className}>
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            style={{ display: 'none' }}
+            src="https://www.facebook.com/tr?id=1287535843253689&ev=PageView&noscript=1"
+            alt=""
+          />
+        </noscript>
         <ToastProvider>
           <AuthProvider>
             <CartProvider>
